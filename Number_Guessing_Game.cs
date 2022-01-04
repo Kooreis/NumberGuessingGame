1d259104763a1591842e439034ed6987dcad99a6
Kooreis <kooreis.official@gmail.com>
2022-01-03 19:00:00 -0600

    Initial setup of the program. Declares and initializes the random number generator, the secret number, the attempts counter, and the boolean flag for guessing.

--- Number_Guessing_Game.cs
+++ Number_Guessing_Game.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 
 class Program
@@ -9,38 +8,5 @@ class Program
         int secretNumber = random.Next(1, 101);
         int attempts = 0;
         bool isGuessed = false;
-
-        Console.WriteLine("Welcome to the Number Guessing Game!");
-        Console.WriteLine("Guess a number between 1 and 100:");
-
-        while (!isGuessed)
-        {
-            string userInput = Console.ReadLine();
-            int userGuess;
-
-            if (int.TryParse(userInput, out userGuess))
-            {
-                attempts++;
-
-                if (userGuess > secretNumber)
-                {
-                    Console.WriteLine("Too high! Try again:");
-                }
-                else if (userGuess < secretNumber)
-                {
-                    Console.WriteLine("Too low! Try again:");
-                }
-                else
-                {
-                    Console.WriteLine($"Congratulations! You've guessed the number in {attempts} attempts.");
-                    isGuessed = true;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid input! Please enter a number:");
-            }
-        }
     }
 }
-```
