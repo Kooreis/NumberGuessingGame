0dd9455198ab92f0c9fcaa15fa29a84a70d9c768
Kooreis <kooreis.official@gmail.com>
2022-01-04 20:00:00 -0600

    Defined the main class and main method. Initialized the Random and Scanner objects, and defined the necessary variables for the game.

--- Number_Guessing_Game.java
+++ Number_Guessing_Game.java
@@ -1,2 +1,8 @@
-import java.util.Random;
-import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int numberToGuess = rand.nextInt(100);
+        int numberOfTries = 0;
+        Scanner input = new Scanner(System.in);
+        int guess;
+        boolean win = false;
